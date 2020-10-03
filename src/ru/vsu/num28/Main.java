57f49c3f776f485d0cfecac64a5999fe3aa75098
Egor Kirin <egorcom27@mail.ru>
2020-10-03 11:32:54 +0300

    task2

--- src/ru/vsu/num28/Main.java
+++ src/ru/vsu/num28/Main.java
@@ -5,36 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int sum = readMoney("amount —> ");
-        int price1 = readMoney("price of 1 cake —> ");
-        int num1 = readMoney("number of 1 cakes —> ");
-        int price2 = readMoney("price of 2 cake —> ");
-        int num2 = readMoney("number of 2 cakes —> ");
+        int sum = readingMoneyAndCakeAmount("amount —> ");
+        int price1 = readingMoneyAndCakeAmount("price of 1 cake —> ");
+        int quantity1 = readingMoneyAndCakeAmount("number of 1 cakes —> ");
+        int price2 = readingMoneyAndCakeAmount("price of 2 cake —> ");
+        int quantity2 = readingMoneyAndCakeAmount("number of 2 cakes —> ");
 
-        checkCake1(price1, price2, sum, num1, num2);
-        checkCake2(price1, price2, sum, num1, num2);
+        checkingTheCostOfCake1(price1, price2, sum, quantity1, quantity2);
+        checkingTheCostOfCake2(price1, price2, sum, quantity1, quantity2);
     }
 
-    private static void checkCake1 (int price1, int price2, int sum, int num1, int num2) {
+    private static void checkingTheCostOfCake1(int price1, int price2, int sum, int quantity1, int quantity2) {
         // Remaining balance
         int bal;
 
         int price = Math.min(price1, price2);
 
         if (price == price1) {
-            if (sum >= price && sum <= price * num1) {
+            if (sum >= price && sum <= price * quantity1) {
                 System.out.print("You bought —> ");
                 bal = sum / price;
                 System.out.println(bal);
             }
-            if (sum > price * num1 && sum <= price * num1 + price2 * num2) {
+            if (sum > price * quantity1 && sum <= price * quantity1 + price2 * quantity2) {
                 System.out.print("You bought —> ");
-                bal = num1 + ((sum - price * num1) / price2);
+                bal = quantity1 + ((sum - price * quantity1) / price2);
                 System.out.println(bal);
             }
-            if (sum > price * num1 + price2 * num2) {
+            if (sum > price * quantity1 + price2 * quantity2) {
                 System.out.print("You bought —> ");
-                bal = num1 + num2;
+                bal = quantity1 + quantity2;
                 System.out.println(bal);
             }
             if (sum < price) {
@@ -43,7 +43,7 @@ public class Main {
         }
     }
 
-    private static void checkCake2 (int price1, int price2, int sum, int num1, int num2) {
+    private static void checkingTheCostOfCake2(int price1, int price2, int sum, int quantity1, int quantity2) {
         // Remaining balance
         int bal;
 
@@ -53,26 +53,26 @@ public class Main {
             if (sum < price) {
                 System.out.println("You won't buy anything");
             }
-            if (sum >= price && sum <= price * num2) {
+            if (sum >= price && sum <= price * quantity2) {
                 System.out.print("You bought --> ");
                 bal = sum / price;
                 System.out.println(bal);
             }
-            if (sum > price * num2 && sum <= price * num2 + price1 * num1) {
+            if (sum > price * quantity2 && sum <= price * quantity2 + price1 * quantity1) {
                 System.out.print("You bought --> ");
-                bal = num2 + ((sum - price * num2) / price1);
+                bal = quantity2 + ((sum - price * quantity2) / price1);
                 System.out.println(bal);
             }
-            if (sum > price * num2 + price1 * num1) {
+            if (sum > price * quantity2 + price1 * quantity1) {
                 System.out.print("You bought --> ");
-                bal = num1 + num2;
+                bal = quantity1 + quantity2;
                 System.out.println(bal);
             }
         }
     }
 
 
-    public static int readMoney(String name) {
+    static int readingMoneyAndCakeAmount(String name) {
         System.out.printf("Enter the %s", name);
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
